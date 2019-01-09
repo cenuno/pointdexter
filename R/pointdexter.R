f4557e4d8da45c87f7e5b7951d4c713501c8a5b3
cenuno <24255819+cenuno@users.noreply.github.com>
2019-01-08 20:00:33 -0600

    Using roxygen2 .Rd file for pointdexter documentation. Removing manual written one.

--- R/pointdexter.R
+++ R/pointdexter.R
@@ -3,11 +3,17 @@
 #' The pointdexter package labels coordinate pairs located inside a polygon.
 #' For a singular polygon, the label is a logical vector of TRUE and FALSE values.
 #' For multiple polygons, the label is a character vector based on the names of each polygon.
-#' The package is designed to work with both 'sf' and SpatialPolygonsDataFrame objects.
+#' The package is designed to work with both \href{https://r-spatial.github.io/sf/}{\code{sf}} and
+#'  \href{https://www.rdocumentation.org/packages/sp/versions/1.2-5/topics/SpatialPolygonsDataFrame-class}{\code{SpatialPolygonsDataFrame}} objects.
+#'
+#' To learn more about dplyr, start with the vignette:
+#' \code{browseVignettes(package = "pointdexter")}
 #'
 #' @section pointdexter functions:
-#' GetPolygonBoundaries():      obtains the boundaries of the polygon(s)
-#' LabelPointsWithinPolygons(): labels points located a polygon
+#' \describe{
+#' \item{\code{GetPolygonBoundaries()}}{obtains the boundaries of the polygon(s)}
+#' \item{\code{LabelPointsWithinPolygons()}}{labels points located a polygon}
+#' }
 #'
 #' @docType package
 #' @name pointdexter
